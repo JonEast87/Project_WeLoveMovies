61c8774487acace8beb83751176744d66dd4bd97
Jon Eastin <picardncce@gmail.com>
2022-08-04 15:28:22 -0700

    adding cors() to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const moviesRouter = require('./movies/movies.router')
@@ -8,6 +9,7 @@ const theatersRouter = require('./theaters/theaters.router')
 const notFound = require('../src/errors/notFound')
 const errorHandler = require('../src/errors/errorHandler')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/movies', moviesRouter)
