89ee864a4068884a30b9191c835104dd0abca5a2
Jon Eastin <picardncce@gmail.com>
2022-08-08 18:27:26 -0700

    removed cors() from the router files

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,9 +2,6 @@ const router = require('express').Router()
 const controller = require('./movies.controller')
 // --- methodNotAllowed is a catch all handler that will return messages for unsupported methods --- //
 const methodNotAllowed = require('../errors/methodNotAllowed')
-const cors = require('cors')
-
-router.use(cors())
 
 router.route('/:movieId').get(controller.read).all(methodNotAllowed)
 
